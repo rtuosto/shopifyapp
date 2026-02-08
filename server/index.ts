e75a57fa7f4135400ff4be228e5fe0f026a034a1
Ryan Tuosto <ryantuosto@gmail.com>
2026-02-08 06:43:27 -0800

    more deployment logging fixes

--- server/index.ts
+++ server/index.ts
@@ -118,9 +118,8 @@ app.use((req, res, next) => {
     app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
       const status = err.status || err.statusCode || 500;
       const message = err.message || "Internal Server Error";
-
-      res.status(status).json({ message });
-      throw err;
+      console.error("[express] Error:", message, err.stack ?? "");
+      if (!res.headersSent) res.status(status).json({ message });
     });
 
     // importantly only setup vite in development and after
